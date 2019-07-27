2a27b9f5674e308faf499af1fb37758ad840a51f
Nancy Lee <srlee713@gmail.com>
2019-07-26 21:28:52 -0600

    Add methods and tests in Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,18 +4,50 @@ class Enigma
 
   def initialize
     @char_set = ("a".."z").to_a << " "
+    @key = Key.new
+    @offset = Offset.new
+    @shift = Shift.new(@key, @offset)
+  end
+
+  def generate_transposed_chars(string, key, date)
+    @key.string = key
+    @offset.date = date
+    shift = Shift.new(@key, @offset)
+    temp_arr = shift.arrange_chars_by_shift(string)
+    hash = {:A => 0, :B => 1, :C => 2, :D => 3}
+
+    hash.each do |k, v|
+      temp_arr[v].map! do |char|
+        next if char == 0
+        @char_set[(@char_set.index(char) + shift.generate_final_shift[k]) % 27]
+      end
+    end
+    temp_arr
+  end
+
+  def generate_encrypted_string(string, key, date)
+    temp_arr = generate_transposed_chars(string, key, date)
+    result = ""
+    arr_size = temp_arr.length
+    iterator = temp_arr[0].length
+
+    iterator.times do |j|
+      arr_size.times do |i|
+        if temp_arr[i][j] == nil
+          break
+        else
+         result += temp_arr[i][j]
+       end
+      end
+    end
+    result
   end
 
   def encrypt(string, key, date)
-    temp_arr = string.split("")
-    shifted_arr = []
-    shift = Shift.new(key, date)
-
-    temp_arr.map do |i|
-      shifted_arr << char_set[temp_arr.index(i) + shift.generate_final_shift]
-    end
-
-    shifted_arr.join
+   {
+      encryption: generate_encrypted_string(string, key, date),
+      key: key,
+      date: date
+    }
   end
-
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,14 +14,22 @@ class EnigmaTest < Minitest::Test
     assert_equal 27, @enigma.char_set.length
   end
 
+  def test_generate_transposed_chars
+    expected = [["k", "r", "u"], ["e", " ", "l"], ["d", "o", "w"], ["e", "h", nil]]
+
+    assert_equal expected, @enigma.generate_transposed_chars("hello world", "02715", "040895")
+  end
+
+  def test_generate_encrypted_string
+    assert_equal "keder ohulw", @enigma.generate_encrypted_string("hello world", "02715", "040895")
+  end
+
   def test_encrypt
-    skip
     expected = {
       encryption: "keder ohulw",
       key: "02715",
       date: "040895"
     }
-
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
 end
