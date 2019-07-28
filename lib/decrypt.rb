8e6ab2f73000cdbafa57fcdad407ec5203c78e36
Nancy Lee <srlee713@gmail.com>
2019-07-27 19:48:12 -0600

    Edit the first parameter in decrypt method

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,14 +1,13 @@
 require './test/test_helper'
 
 handle = File.open(ARGV[0], "r")
-message = handle.read
+encrypted = handle.read
 handle.close
 
 enigma = Enigma.new
 key = "82648"
 date = "240818"
-encrypted = enigma.encrypt(message, key, date)
-decrypted_text = enigma.decrypt(encrypted[:encryption], key, date)[:decryption]
+decrypted_text = enigma.decrypt(encrypted, key, date)[:decryption]
 
 writer = File.open(ARGV[1], "w")
 writer.write(decrypted_text)
