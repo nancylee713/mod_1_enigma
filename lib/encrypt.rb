8197167eb3c76f52c3961935b14b7980bc56a419
Nancy Lee <srlee713@gmail.com>
2019-07-30 15:39:01 -0600

    Change string-interpolated values in encrypt file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -8,9 +8,11 @@ enigma = Enigma.new
 key = "82648"
 date = "240818"
 encrypted_text = enigma.encrypt(message, key, date)[:encryption]
+encrypted_key = enigma.encrypt(message, key, date)[:key]
+encrypted_date = enigma.encrypt(message, key, date)[:date]
 
 writer = File.open(ARGV[1], "w")
 writer.write(encrypted_text)
 writer.close
 
-puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
+puts "Created #{ARGV[1]} with the key #{encrypted_key} and date #{encrypted_date}"
